c02f5eb9411cebe0f9588415f7c5d7d4e6d6f785
krobnson <krobnson@gmail.com>
2016-05-30 17:27:01 -0700

    Calendar and event 1.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :calendar, only: [:show]
+  resources :events, :path => 'events'
   resources :posts
   resources :employees, only: [:new, :create]
   get 'about' => 'welcome#about'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160529210916) do
+ActiveRecord::Schema.define(version: 20160530231028) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20160529210916) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.date     "ocurrs_on"
   end
 
   create_table "posts", force: :cascade do |t|
